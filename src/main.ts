3cb840bd94ad5bd904421c9427188fd48b9f3658
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-08-09 12:57:56 +0100

    added morse code translator functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./main.scss";
+import { convertToMorse } from "./utils/morseCodeTranslator";
+
+const input = document.querySelector(".input-form__input") as HTMLElement;
+const morseCode = document.querySelector(".morse-code__output");
+
+if (!input || !morseCode) throw new Error("Issue with selector");
+
+const handleInput = (event: Event) => {
+  const userInput: string = (event.currentTarget as HTMLInputElement).value;
+
+  morseCode.innerHTML = convertToMorse(userInput);
+};
+
+input.addEventListener("input", handleInput);
